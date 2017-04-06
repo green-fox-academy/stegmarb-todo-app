7ad8e44a50a6e02b98dca8603dacf8595d51cb96
Balazs Stegmar <stegmarb@gmail.com>
2017-04-06 23:20:18 +0200

    feat(TodoApp): methods now outsourced into new class

--- TodoApp/src/Main.java
+++ TodoApp/src/Main.java
@@ -1,180 +1,26 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-  public final static String SEPARATOR = ";";
-  public final static String TASKPATH = "Tasks.txt";
-
   public static void main(String[] args) {
+    TodoMethods todoApp = new TodoMethods();
     if (args.length == 0){
-      printUsage();
+      todoApp.printUsage();
     } else if (args[0].equals("-la")) {
-      printAllTasks();
+      todoApp.printAllTasks();
     } else if (args[0].equals("-l")) {
-      printPendingTasks();
+      todoApp.printPendingTasks();
     } else if (args[0].equals("-a") && args.length == 2) {
-      addTask(args[1]);
+      todoApp.addTask(args[1]);
     } else if (args[0].equals("-r")) {
-      try {
-        removeTask(Integer.parseInt(args[1]));
-      } catch (IndexOutOfBoundsException iob) {
-        System.out.println("Index is out of bounds");
-      } catch (NumberFormatException nfe) {
-        System.out.println("Index is not a number");
-      }
+      todoApp.removeTask(Integer.parseInt(args[1]));
     } else if (args[0].equals("-c")) {
-      try {
-        nowIsDone(Integer.parseInt(args[1]));
-      } catch (IndexOutOfBoundsException iob) {
-        System.out.println("Index is out of bounds");
-      } catch (NumberFormatException nfe) {
-        System.out.println("Index is not a number");
-      }
+      todoApp.nowIsDone(Integer.parseInt(args[1]));
     } else if (args[0].equals("-s")) {
-      try {
-        checkStatus(Integer.parseInt(args[1]));
-      } catch (IndexOutOfBoundsException iob) {
-        System.out.println("Index is out of bounds");
-      } catch (NumberFormatException nfe) {
-        System.out.println("Index is not a number");
-      }
+      todoApp.checkStatus(Integer.parseInt(args[1]));
     } else if (args[0].equals("-h")) {
-      printHelp();
+      todoApp.printHelp();
     } else {
-      System.out.println("Oops!! Unable to perform the action because of the wrong input format. Please check the \"-h\" option for correct format recommendations.");
-    }
-  }
-
-  public static void printUsage() {
-    Path usage = Paths.get("Usage.txt");
-    try {
-      List<String> usageText = Files.readAllLines(usage);
-      for (int i = 0; i < usageText.size(); i++) {
-        System.out.println(usageText.get(i));
-      }
-    } catch (IOException e) {
-      System.out.println("Something wrong with usage file");
-    }
-  }
-
-  public static void printAllTasks() {
-    Path tasks = Paths.get(TASKPATH);
-    try {
-      List<String> taskList = Files.readAllLines(tasks);
-      if (taskList.size() == 0) {
-        System.out.println("No todos for today! :)");
-      } else {
-        for (int i = 0; i < taskList.size(); i++) {
-          System.out.println(i + 1 + " - " + isDoneToString(taskList.get(i)) + " " + onlyTask(taskList.get(i)));
-        }
-      }
-    } catch (IOException e) {
-      System.out.println("Something wrong with tasks file");
-    }
-  }
-
-  public static void printPendingTasks() {
-    Path tasks = Paths.get(TASKPATH);
-    try {
-      List<String> taskList = Files.readAllLines(tasks);
-      List<String> pendingTasks = new ArrayList<>();
-      for (int i = 0; i < taskList.size(); i++) {
-        String[] subList = taskList.get(i).split(SEPARATOR);
-        if (subList[1].equals("pending")) {
-          pendingTasks.add(taskList.get(i));
-        }
-      }
-      Files.write(Paths.get("PendingTasks.txt"), pendingTasks);
-      for (int i = 0; i < pendingTasks.size(); i++) {
-        System.out.println(i + 1 + " - " + isDoneToString(pendingTasks.get(i)) + " " + onlyTask(pendingTasks.get(i)));
-      }
-    }catch(IOException e) {
-      System.out.println("Something wrong with tasks file");
-    }
-  }
-
-  public static void addTask(String task) {
-    Path tasks = Paths.get(TASKPATH);
-    try {
-      List<String> taskList = Files.readAllLines(tasks);
-      String finalTask = task + ";pending";
-      taskList.add(finalTask);
-      Files.write(tasks, taskList);
-    } catch (IOException e) {
-      System.out.println("Something wrong with tasks file");
-    }
-  }
-
-  public static void removeTask(int task) {
-    Path tasks = Paths.get(TASKPATH);
-    try {
-      List<String> taskList = Files.readAllLines(tasks);
-      if (taskList.size() <= 2) {
-        return;
-      } else {
-        taskList.remove(task - 1);
-      }
-      Files.write(tasks, taskList);
-    } catch (IOException e) {
-      System.out.println("Something wrong with tasks file");
-    }
-  }
-
-  public static String isDoneToString(String task) {
-    String[] subList = task.split(SEPARATOR);
-    if (subList[1].equals("done")) {
-      return "[X]";
-    } else {
-      return "[ ]";
-    }
-  }
-
-  public static void nowIsDone(int orderNumber) {
-    Path tasks = Paths.get(TASKPATH);
-    try {
-      List<String> taskList = Files.readAllLines(tasks);
-      String[] subList = taskList.get(orderNumber-1).split(SEPARATOR);
-      subList[1] = "done";
-      taskList.set(orderNumber-1, subList[0] + ";" + subList[1]);
-      Files.write(tasks, taskList);
-    } catch (IOException e) {
-      System.out.println("Something wrong with tasks file");
-    }
-  }
-
-  public static void checkStatus(int orderNumber) {
-    Path tasks = Paths.get(TASKPATH);
-    try {
-      List<String> taskList = Files.readAllLines(tasks);
-      String[] subList = taskList.get(orderNumber - 1).split(SEPARATOR);
-      if (subList[1].equals("done")) {
-        System.out.println("The \"" + subList[0] + "\" task is done!");
-      } else {
-        System.out.println("The \"" + subList[0] + "\" task is pending!");
-      }
-    } catch (IOException e) {
-      System.out.println("Something wrong with tasks file");
-    }
-  }
-
-  public static String onlyTask(String task) {
-    String[] subTasks = task.split(SEPARATOR);
-    return subTasks[0];
-  }
-
-  public static void printHelp() {
-    Path usage = Paths.get("Help.txt");
-    try {
-      List<String> usageText = Files.readAllLines(usage);
-      for (int i = 0; i < usageText.size(); i++) {
-        System.out.println(usageText.get(i));
-      }
-    } catch (IOException e) {
-      System.out.println("Something wrong with help file");
+      System.out.println("Oops!! Unable to perform the action because of the wrong input format.\n" +
+              "Please check the \"-h\" option for correct format recommendations.");
     }
   }
 }
+
---
+++ TodoApp/src/TodoMethods.java
@@ -0,0 +1,158 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoMethods {
+  public final static String SEPARATOR = ";";
+  public final static String TASKPATH = "Tasks.txt";
+
+  public void printUsage() {
+    Path usage = Paths.get("Usage.txt");
+    try {
+      List<String> usageText = Files.readAllLines(usage);
+      for (int i = 0; i < usageText.size(); i++) {
+        System.out.println(usageText.get(i));
+      }
+    } catch (IOException e) {
+      System.out.println("Something wrong with usage file");
+    }
+  }
+
+  public void printAllTasks() {
+    Path tasks = Paths.get(TASKPATH);
+    try {
+      List<String> taskList = Files.readAllLines(tasks);
+      if (taskList.size() == 0) {
+        System.out.println("No todos for today! :)");
+      } else {
+        for (int i = 0; i < taskList.size(); i++) {
+          System.out.println(i + 1 + " - " + isDoneToString(taskList.get(i)) + " " + onlyTask(taskList.get(i)));
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("Something wrong with tasks file");
+    }
+  }
+
+  public void printPendingTasks() {
+    Path tasks = Paths.get(TASKPATH);
+    try {
+      List<String> taskList = Files.readAllLines(tasks);
+      List<String> pendingTasks = new ArrayList<>();
+      for (int i = 0; i < taskList.size(); i++) {
+        String[] subList = taskList.get(i).split(SEPARATOR);
+        if (subList[1].equals("pending")) {
+          pendingTasks.add(taskList.get(i));
+        }
+      }
+      Files.write(Paths.get("PendingTasks.txt"), pendingTasks);
+      for (int i = 0; i < pendingTasks.size(); i++) {
+        System.out.println(i + 1 + " - " + isDoneToString(pendingTasks.get(i)) + " " + onlyTask(pendingTasks.get(i)));
+      }
+    }catch(IOException e) {
+      System.out.println("Something wrong with tasks file");
+    }
+  }
+
+  public void addTask(String task) {
+    Path tasks = Paths.get(TASKPATH);
+    try {
+      List<String> taskList = Files.readAllLines(tasks);
+      String finalTask = task + ";pending";
+      taskList.add(finalTask);
+      Files.write(tasks, taskList);
+    } catch (IOException e) {
+      System.out.println("Something wrong with tasks file");
+    }
+  }
+
+  public void removeTask(int task) {
+    try {
+      Path tasks = Paths.get(TASKPATH);
+      try {
+        List<String> taskList = Files.readAllLines(tasks);
+        if (taskList.size() <= 2) {
+          return;
+        } else {
+          taskList.remove(task - 1);
+        }
+        Files.write(tasks, taskList);
+      } catch (IOException e) {
+        System.out.println("Something wrong with tasks file");
+      }
+    } catch (IndexOutOfBoundsException iob) {
+      System.out.println("Index is out of bounds");
+    } catch (NumberFormatException nfe) {
+      System.out.println("Index is not a number");
+    }
+  }
+
+  public String isDoneToString(String task) {
+    String[] subList = task.split(SEPARATOR);
+    if (subList[1].equals("done")) {
+      return "[X]";
+    } else {
+      return "[ ]";
+    }
+  }
+
+  public void nowIsDone(int orderNumber) {
+    try {
+      Path tasks = Paths.get(TASKPATH);
+      try {
+        List<String> taskList = Files.readAllLines(tasks);
+        String[] subList = taskList.get(orderNumber - 1).split(SEPARATOR);
+        subList[1] = "done";
+        taskList.set(orderNumber - 1, subList[0] + ";" + subList[1]);
+        Files.write(tasks, taskList);
+      } catch (IOException e) {
+        System.out.println("Something wrong with tasks file");
+      }
+    } catch (IndexOutOfBoundsException iob) {
+      System.out.println("Index is out of bounds");
+    } catch (NumberFormatException nfe) {
+      System.out.println("Index is not a number");
+    }
+  }
+
+  public void checkStatus(int orderNumber) {
+    try {
+      Path tasks = Paths.get(TASKPATH);
+      try {
+        List<String> taskList = Files.readAllLines(tasks);
+        String[] subList = taskList.get(orderNumber - 1).split(SEPARATOR);
+        if (subList[1].equals("done")) {
+          System.out.println("The \"" + subList[0] + "\" task is done!");
+        } else {
+          System.out.println("The \"" + subList[0] + "\" task is pending!");
+        }
+      } catch (IOException e) {
+        System.out.println("Something wrong with tasks file");
+      }
+    } catch (IndexOutOfBoundsException iob) {
+      System.out.println("Index is out of bounds");
+    } catch (NumberFormatException nfe) {
+      System.out.println("Index is not a number");
+    }
+  }
+
+  public String onlyTask(String task) {
+    String[] subTasks = task.split(SEPARATOR);
+    return subTasks[0];
+  }
+
+  public void printHelp() {
+    Path usage = Paths.get("Help.txt");
+    try {
+      List<String> usageText = Files.readAllLines(usage);
+      for (int i = 0; i < usageText.size(); i++) {
+        System.out.println(usageText.get(i));
+      }
+    } catch (IOException e) {
+      System.out.println("Something wrong with help file");
+    }
+  }
+}
